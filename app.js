a3791bbdf387d35988cfc1f0d7d4e130365d8425
vizukasu <152366651+vizukasu@users.noreply.github.com>
2026-06-14 21:45:00 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 function gcd(a, b) {
-    while (b) [a, b] = [b, a % b];
+    while (b) {
+        [a, b] = [b, a % b];
+    }
     return a;
 }
 
 function lcm(a, b) {
-    return (a * b) / gcd(a, b);
+    const bigA = BigInt(a);
+    const bigB = BigInt(b);
+    const bigG = BigInt(gcd(a, b));
+    return String(bigA * bigB / bigG);
 }
 
-app.get('/', (req, res) => {
-    const x = Number(req.query.x);
-    const y = Number(req.query.y);
+app.get('/skemetrtalgat_gmail_com', (req, res) => {
+    const x = req.query.x;
+    const y = req.query.y;
 
-    if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0) {
+    const xNum = Number(x);
+    const yNum = Number(y);
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (
+        !x || !y ||
+        !Number.isInteger(xNum) || !Number.isInteger(yNum) ||
+        xNum <= 0 || yNum <= 0
+    ) {
         return res.send('NaN');
     }
 
-    res.send(String(lcm(x, y)));
+    res.send(lcm(xNum, yNum));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log(`Server running on port ${PORT}`);
 });
